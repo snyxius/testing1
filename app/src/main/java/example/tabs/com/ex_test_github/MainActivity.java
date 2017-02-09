2703905e24e2eda940d983ed606aa6cd5ed7f380
Snyxius Technologies <support@snyxius.com>
2017-02-09 18:36:38 +0530

    initial commet

---
+++ app/src/main/java/example/tabs/com/ex_test_github/MainActivity.java
@@ -0,0 +1,13 @@
+package example.tabs.com.ex_test_github;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
